a614df227244e1610e787c06397c9ccd17c65821
mariangela <mari.escobar.marcondes@hotmail.com>
2016-05-31 12:54:32 +0000

    Rafaela Panuccio - alteração principal.css

--- view/home.php
+++ view/home.php
@@ -1,17 +1,17 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Ticket Happy - Home</title>
 	<link rel="shortcut icon" href="imagens/logo/favicon.ico" >
-		<!-- Latest compiled and minified CSS -->
+	
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     
-    <link href="css/principal.css" rel="stylesheet">
+    <link href="principal.css" rel="stylesheet">
 </head>
 
 <body>
