5ccb2addf53eab16ca548d5d89b9fbcf56118be9
mariangela <mari.escobar.marcondes@hotmail.com>
2016-05-31 17:30:31 +0000

    Rafaela Panuccio: alteração no header página compra_rei_leao

---
+++ view/compra_rei_leao.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ticket Happy - Carrinho de Compras</title>
+	<link rel="shortcut icon" href="imagens/logo/favicon.ico" >
+	
+	<!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    
+    <link href="css/principal.css" rel="stylesheet">
+	
+</head>
+
+<body>
+<div id="wrapper">
+
+    <!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			 
+			  <a class="navbar-brand" href="home.html">
+				<img alt="Brand" src="imagens/logo/2.png" class="img-responsive">
+			  </a>
+			</div>
+
+			<!-- MENU -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
+				<li><a href="home.html"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+				
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-triangle-bottom"></span> Teatro</a>
+				  <ul class="dropdown-menu" role="menu">
+					<li><a href="teatro_musical.html">Musical</a></li>
+					<li><a href="teatro_comedia.html">Comédia</a></li>
+					<li><a href="teatro_infantil.html">Infantil</a></li>									
+				  </ul>
+				</li>
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-triangle-bottom"></span> Shows</a>
+				  <ul class="dropdown-menu" role="menu">
+					<li><a href="shows_nacional.html">Nacional</a></li>
+					<li><a href="shows_internacional.html">Internacional</a></li>
+				  </ul>
+				</li>
+				
+				<li><a href="festivais.html"><span class="glyphicon glyphicon-triangle-right"></span>Festivais</a></li>
+				<li><a href="orquestra_danca.html"><span class="glyphicon glyphicon-triangle-right"></span>Orquestra & Dança</a></li>
+				<li><a href="exposicoes.html"><span class="glyphicon glyphicon-triangle-right"></span>Exposições</a></li>
+				<li><a href="especiais.html"><span class="glyphicon glyphicon-triangle-right"></span>Especiais</a></li>
+				
+			  </ul>
+			  
+			   <!-- LOGIN -->	
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a href="login.html"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+				
+			  </ul>
+			  
+			  <!-- FORM => SEARCH -->
+			  <form class="navbar-form navbar-right" role="search">
+				<div class="form-group">				  
+				  <input type="text" class="form-control cor_search" placeholder="Digite...">
+				</div>				
+				<button type="submit" class="btn btn-search"><span class="glyphicon glyphicon-search"></span></button>
+			  </form>
+			  
+			 
+			</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	<!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+	
+	<div class="div_breadcrumb">
+		<!--------------------------- BREADCRUMB  --------------------------------------> 
+		<ol class="breadcrumb">
+			<li><a href="home.html">Home</a></li>
+			<li><a href="login.html">Login</a></li>
+			<li class="active">Carrinho de Compras</li>				
+		</ol>
+		<!--------------------------- BREADCRUMB --------------------------------------> 
+	</div>	
+	
+	<div id="content">		
+		
+		<!------------------------------ MAIN --------------------------------------------->
+		<main id="largura_compra">
+			<h2>Resumo do Pedido</h2>
+			
+			<table id="cart" class="table table-condensed">
+					<thead class="thead_compra">
+						<tr>
+							<th style="width:50%">Produto</th>
+							<th style="width:10%">Preço</th>
+							<th style="width:8%">Quantidade</th>
+							<th style="width:22%" class="text-center">Subtotal</th>
+							<th style="width:10%"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="tr_compra">
+							<td data-th="Produto">
+								<div class="row">
+									<div class="col-sm-2 hidden-xs"><img src="imagens/aside/rei-leao.jpg" alt="..." class="img-responsive"/></div>
+									<div class="col-sm-10">
+										<h4 class="nomargin">Ingresso Rei Leão - o musical</h4>
+									</div>
+								</div>
+							</td>
+							
+						  <td data-th="Preco">$1.99</td>
+							<td data-th="Quantidade">
+								<input type="number" class="form-control text-center" value="1">
+							</td>	
+						  <td data-th="Subtotal" class="text-center">1.99</td>
+							<td class="actions" data-th="">
+								<button class="btn btn-laranja btn-sm"><i class="fa fa-refresh"></i></i><span class="glyphicon glyphicon-refresh"></button>
+								<button class="btn btn-laranja btn-sm"><i class="fa fa-trash-o"></i><span class="glyphicon glyphicon-trash"></span></button>								
+							</td>
+						</tr>
+					</tbody>
+						<tfoot>
+						<tr>
+							<td><a href="home.html" class="btn btn-laranja btn-warning"><i class="fa fa-angle-left"></i> Continuar Comprando</a></td>
+							<td colspan="2" class="hidden-xs"></td>
+							<td><div class="form-group">
+								<label for="Total" class="col-sm-2 control-label color_label">Total:</label>
+									<div class="col-sm-6">
+										<input type="total" class="form-control" id="total">
+									</div>
+							</div></td>
+							<td><a href="compra.html" class="btn btn-laranja btn-block">Comprar <i class="fa fa-angle-right"></i></a></td>
+						</tr>
+					</tfoot>
+				</table>
+		</main>
+		<!------------------------------ MAIN --------------------------------------------->
+		
+		
+		
+	</div>
+	
+	
+	<!----------------------------------------------- FOOTER -------------------------------------------------------------------->
+	 <!-- FOOTER -->
+	 <footer id="footer" class="clearfix">
+      <div id="footer-widgets">
+
+        <div class="container">
+
+        <div id="footer-wrapper">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Serviços</h4>
+					<ul>
+						<li><a href="contato.html">Contato</a></li>
+						<li><a href="#">Central de Vendas</a></li>
+						<li><a href="#" title="">Vendas para Grupos</a></li>
+						<li><a href="#" title="">Vendas pela Internet</a></li>
+						<li><a href="#" title="">Vantagens do Sistema</a></li>						
+					</ul>
+				</div>            
+			</div> <!-- end widget1 -->
+
+			<!-- 4 -->
+            <div class="col-sm-6 col-md-3">
+				<div id="search-3" class="widget widgetFooter widget_search"><h4 class="widgettitle">Mídias Sociais</h4>
+				<div class="form-inline">				
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/1.jpg" alt="Imagem icone Facebook"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/2.jpg" alt="Imagem icone Twitter"></a>								
+						</div>	
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/3.jpg" alt="Imagem icone Google +"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/4.jpg" alt="Imagem icone Youtube"></a>								
+						</div>	
+				</div>
+				</div>          
+			</div> <!-- end 4 -->
+
+          </div> <!-- end .row -->
+
+        </div> <!-- end #footer-wrapper -->
+
+        </div> <!-- end .container -->
+      </div> <!-- end #footer-widgets -->
+
+		<div id="sub-floor">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 copyright">
+						tickethappy.com.br 2015 © 
+					</div>	
+
+					<div class="col-md-4 col-md-offset-4 attribution">
+						Desenvolvido por:<a href="#"> DMR</a>	
+					</div>
+
+				</div> <!-- end .row -->
+			</div>
+		</div>
+
+    </footer> <!-- end footer -->
+	<!-- FOOTER -->
+
+
+</div><!--Wrapper -->
+    
+
+    
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	
+  </body>
+</html>
