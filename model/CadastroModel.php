2af355f26e05efd3f556a80eb4ae875af1f06a60
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-02 00:03:48 +0000

    Mariangela: Criacao do CadastroModel.php com suas funcoes

---
+++ model/CadastroModel.php
@@ -0,0 +1,113 @@
+<?php
+
+class CadastroModel{
+    
+    //campos da tabela eventos
+    private $id, 
+            $nome, 
+            $email,
+            $cpf,
+            $dt_nasc, 
+            $telefone,
+            $celular, 
+            $sexo, 
+            $endereco,
+            $numero,
+            $apartamento,
+            $bairro,
+            $cidade, 
+            $estado,
+            $cep,
+            $senha;
+            
+                                   
+    
+    public function __construct($id, $nome, $email, $cpf, $dt_nasc, $telefone,
+                                $celular, $sexo, $endereco, $numero, $apartamento,
+                                $bairro, $cidade, $estado, $cep, $senha)
+    {
+        $this->id           = $id;
+        $this->nome         = $nome;
+        $this->email        = $email;
+        $this->cpf          = $cpf;
+        $this->dataNasc     = $dt_nasc;
+        $this->telefone     = $telefone;
+        $this->celular      = $celular;
+        $this->sexo         = $sexo;
+        $this->endereco     = $endereco;
+        $this->numero       = $numero;
+        $this->apartamento  = $apartamento;
+        $this->bairro       = $bairro;
+        $this->cidade       = $cidade;
+        $this->estado       = $estado;
+        $this->cep          = $cep;
+        $this->senha        = $senha;
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function getNome(){
+        return $this->nome;
+    }
+    
+    public function getEmail(){
+        return $this->email;
+    }
+    
+    public function getCpf(){
+        return $this->cpf;
+    }
+    
+    public function getDataNasc(){
+        return $this->dataNasc;
+    }
+      
+    public function getTelefone(){
+        return $this->telefone;
+    }
+    
+    public function getCelular(){
+        return $this->celular;
+    }
+    
+    public function getSexo(){
+        return $this->sexo;
+    }
+    
+    public function getEndereco(){
+        return $this->endereco;
+    }
+   
+    public function getNumero(){
+        return $this->numero;
+    }
+    
+    public function getApartamento(){
+        return $this->apartamento;
+    }
+    
+    public function getBairro(){
+        return $this->bairro;
+    }
+    
+    public function getCidade(){
+        return $this->cidade;
+    }
+    
+    public function getEstado(){
+        return $this->estado;
+    }
+    
+    public function getCep(){
+        return $this->cep;
+    }
+    
+    public function getSenha){
+        return $this->senha;
+    }
+   
+}
+
+?>
