4d8ddbed1ebaf850152826a20046e390fa7c782a
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 14:31:47 +0000

    Mari: criando footer.php

---
+++ view/footer.php
@@ -0,0 +1,127 @@
+		
+		<!-- FOOTER -->
+	 <footer id="footer" class="clearfix">
+      <div id="footer-widgets">
+
+        <div class="container">
+
+        <div id="footer-wrapper">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><a href="/home/home" title="Link para Home"><h4 class="widgettitle">Home</h4></a>
+					<ul>
+						<li><a href="/contato/cadastroContato" title="Link para Contato">Contato</a></li>
+						<li><a href="/login/formulario" title="Link para Login">Login</a></li>											
+					</ul>
+				</div>            
+			</div> <!-- end widget1 -->
+			
+			<div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Teatro</h4>
+					<ul>						
+						<li><a href="/home/musical" title="Link para Musical">Musical</a></li>
+						<li><a href="/home/comedia" title="Link para Comédia">Comédia</a></li>
+						<li><a href="/home/infantil" title="Link para Infantil">Infantil</a></li>											
+					</ul>
+				</div>            
+			</div> <!-- end widget1 -->
+			
+			<div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Shows</h4>
+					<ul>
+						<li><a href="/home/nacional" title="Link para Shows Nacionais">Nacional</a></li>
+						<li><a href="/home/internacional" title="Link para Shows Internacionais">Internacional</a></li>																
+					</ul>
+				</div>            
+			</div> <!-- end widget1 -->
+			
+				<div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><a href="/home/festivais" title="Link para Festivais"><h4 class="widgettitle">Festivais</h4></a>						
+				</div> <br>
+				<div id="meta-3" class="widget widgetFooter widget_meta"><a href="/home/orquestraEdanca" title="Link para Orquestra & Dança"><h4 class="widgettitle">Orquestra & Dança</h4></a>					
+				</div> 	<br>
+				<div id="meta-3" class="widget widgetFooter widget_meta"><a href="/home/exposicoes" title="Link para Exposições"><h4 class="widgettitle">Exposições</h4></a>					
+				</div> <br>
+				<div id="meta-3" class="widget widgetFooter widget_meta"><a href="/home/especiais" title="Link para Especiais"><h4 class="widgettitle">Especiais</h4></a>					
+				</div> 	<br>			
+			</div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-3">
+              	<div id="recent-posts-3" class="widget widgetFooter widget_recent_entries"><h4 class="widgettitle">Formas de Pagamento</h4>	
+					<div class="form-inline">				
+						<div class="input-group">							
+							<img src="imagens/icones/pagamento.png" alt="Formas de Pagamento" >						
+						</div>
+						
+				</div>
+				</div>           
+			</div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-3">
+				<div id="meta-4" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Certificados de Segurança</h4>		
+					<div class="form-inline">				
+						<div class="input-group">							
+							<img src="imagens/icones/certificados.png" alt="Cerificados de Segurança">						
+						</div>
+							
+				</div>
+				</div>           
+			</div> <!-- end widget1 -->
+			<!-- 4 -->
+             <div class="col-sm-6 col-md-3">
+				<div id="search-3" class="widget widgetFooter widget_search"><h4 class="widgettitle">Mídias Sociais</h4>
+				<div class="form-inline">				
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/1.jpg" alt="Imagem icone Facebook"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/2.jpg" alt="Imagem icone Twitter"></a>								
+						</div>	
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/3.jpg" alt="Imagem icone Google +"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/4.jpg" alt="Imagem icone Youtube"></a>								
+						</div>	
+				</div>
+				</div>          
+			</div> <!-- end 4 -->
+
+          </div> <!-- end .row -->
+
+        </div> <!-- end #footer-wrapper -->
+
+        </div> <!-- end .container -->
+      </div> <!-- end #footer-widgets -->
+
+		<div id="sub-floor">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 copyright">
+						tickethappy.com.br 2015 © 
+					</div>	
+
+					<div class="col-md-4 col-md-offset-4 attribution">
+						Desenvolvido por: DMR
+					</div>
+
+				</div> <!-- end .row -->
+			</div>
+		</div>
+
+    </footer> <!-- end footer -->
+	<!-- FOOTER -->
+		
+	
+	</div><!-- WRAPPER -->
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>
+         <?php include 'static/js/bootstrap.min.js'; ?>
+    </script>
+	
+	
+</body>
+
+</html>
