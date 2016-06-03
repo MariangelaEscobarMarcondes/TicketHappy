8014e60c0b56e46d53dfc75e81060c4b3c796370
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 19:34:18 +0000

    Mari: arrumando rotas, imgs

---
+++ view/coldplay.php
@@ -0,0 +1,106 @@
+
+	<!-- NAVBAR => BOOSTRAP -->
+	
+	
+	<div id="content_imagem_topo">
+		<div id="content_imagem_topo_principal">	
+			<img src="https://ticket-happy-mariangela.c9users.io/imagens/2-shows/internacional/Coldplay/1-coldplay.jpg" alt="Pesquisa" width="100%">		
+		</div>	
+		
+		<div class="div_breadcrumb div_breadcrumb_cold">
+			<!-- BREADCRUMB  --> 
+			<ol class="breadcrumb">
+				<li><a href="/home/home">Home</a></li>
+				<li><a href="#">Shows</a></li>
+				<li><a href="/home/internacional">Internacional</a></li>
+				<li class="active">Coldplay</li>
+					
+			</ol>
+			<!-- BREADCRUMB --> 
+		</div>	
+	
+		<div id="main_content">
+			<div class="div_info">
+				<h2>Coldplay</h2>
+				
+				<p class="texto_justificado">Banda Coldplay é uma banda britânica de rock alternativo fundada em 1996 na Inglaterra pelo 
+				vocalista principal Chris Martin e o guitarrista Jonny Buckland no University College London.
+				</p>
+				<p><strong>Vocalista: Chris Martin</strong></p>
+				<p><strong>Início da carreira: Londres, Reino Unido</strong></p>
+				<p><strong>Integrantes: Chris Martin, Guy Berryman, Jonny Buckland, Will Champion</strong></p>
+					
+				<h2>Músicas</h2>
+				
+				<table class="table table-condensed">
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>Ano</th>
+							<th>Album</th>
+						</tr>
+					</thead>
+					<tbody>							
+						<tr>
+						  <td class="">A Sky Full of Stars</td>
+						  <td class="">2014</td>
+						  <td class="">Ghost Stories Live 2014 Fix You</td>					  
+						</tr>
+						<tr>
+						  <td class="">Fix You</td>
+						  <td class="">2005</td>
+						  <td class="">XeY</td>					  
+						</tr>
+						<tr>
+						  <td class="">The Scientist</td>
+						  <td class="">2002</td>
+						  <td class="">A Rush of Blood to the Head</td>					  
+						</tr>
+						<tr>
+						  <td class="">Paradise</td>
+						  <td class="">2011</td>
+						  <td class="">Mylo Xyloto</td>					  
+						</tr>
+						<tr>
+						  <td class="">Clocks</td>
+						  <td class="">2002</td>
+						  <td class="">A Rush of Blood to the Head</td>					  
+						</tr>
+						<tr>
+						  <td class="">Viva la Vida</td>
+						  <td class="">2008</td>
+						  <td class="">Viva la Vida or Death and All His Friends</td>					  
+						</tr>
+					</tbody>
+				</table>
+				
+				<h2>INFORMAÇÕES</h2>
+				
+				<p><strong><span class="glyphicon glyphicon-calendar"></span>  Data(s):</strong> 28 e 29 de setembro de 2015</p>
+				<p><strong><span class="glyphicon glyphicon-time"></span>  Horário(s):</strong>  22h</p>
+				<p><strong><span class="glyphicon glyphicon-piggy-bank"></span>  Preço(s):</strong> R$ 290,00 (Meia entrada) – R$ 580,00 (Inteira)</p>
+				<p><strong><span class="glyphicon glyphicon-map-marker"></span>  Onde:</strong> Estádio do Morumbi - São Paulo - SP</p>
+						
+			</div>
+			
+			<!-- Large modal -->
+			<button type="button" class="btn btn-laranja" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-facetime-video"></span>  Video</button>
+			<a href="compra_coldplay.html" class="btn btn-laranja" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Comprar</a>
+			<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+			  <div class="modal-dialog modal-lg">
+				<div class="">					
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						
+						<div class="imagem_video">
+							<video id="video" src="videos/Coldplay.mp4" controls loop muted preload="auto" poster="imagens/2-shows/internacional/Coldplay/Coldplay.jpg" ></video> 	
+						</div>				
+				</div>
+			  </div>
+			</div>
+		
+		
+		</div><!-- CONTENT -->
+		
+	</div>	
+	
+	<!-- FOOTER -->
