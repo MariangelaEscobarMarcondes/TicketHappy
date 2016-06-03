033dd455186eead6251020374676c88ecc3b838a
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 01:35:47 +0000

    Mari: criacao das funcoes getContatos e deleteContato

--- model/ContatoDAO.php
+++ model/ContatoDAO.php
@@ -11,8 +11,7 @@ class ContatoDAO{
         }
         
         $stmt = $mysqli->prepare("INSERT INTO contatos(nm_nomeCompleto,ds_email,ds_assunto,ds_mensagem) VALUES (?,?,?,?)");
-        
-        //Mudar aqui de acordo com a aula de seguranca
+       
         $stmt->bind_param("ssss",$c->getNomeCompleto(),$c->getEmail(),$c->getAssunto(),$c->getMensagem());
         
         //----------------------------------------------
@@ -23,37 +22,21 @@ class ContatoDAO{
         return "";
     }
     
-    //vai selecionar todos os contatos
-    public function getAllContatos(){
-        
+    //listar
+    public function getContatos(){
         $mysqli = new mysqli("127.0.0.1", "mariangela", "", "ticketHappy");
         
         $stmt = $mysqli->prepare("SELECT * FROM contatos");
-        
-        //$stmt->bind_param("i",$_POST["id"]);
-        $stmt->bind_param("i",$id);
         $stmt->execute();
-        $stmt->fetch();
         
-        $row = $stmt->get_result()->fetch_all();//pega todos os resultados e joga num vetor
-        foreach($row as $Contato){
-            return $e[1];
+        $arr = mysqli_fetch_all(mysqli_stmt_get_result($stmt));
+        
+        $contatos = array();
+        
+        foreach($arr as $a){
+            $contatos[] = new ContatoModel($a[0],$a[1],$a[2],$a[3],$a[4]);
         }
-    }
-    
-    public function updateContato($id){
-        
-        $mysqli = new mysqli("127.0.0.1", "mariangela", "", "ticketHappy");
-        
-        $stmt = $mysqli->prepare("PUT INTO contatos(nm_nomeCompleto,ds_email,ds_assunto,ds_mensagem) VALUES (?,?,?,?)");
-        
-        $stmt->bind_param("i",$id);
-        $stmt->execute();
-        $stmt->bind_result($id, $nomeCompleto, $email, $assunto, $mensagem);
-        $stmt->fetch();
-        
-        $evento = new ContatoModel($id, $nomeCompleto, $email, $assunto, $mensagem);
-        return $evento;
+        return $contatos;
     }
     
     public function deleteContato($id){
@@ -68,6 +51,54 @@ class ContatoDAO{
         }
         $stmt->close();
     }
+    
+    
+    
+    //vai selecionar todos os contatos
+    public function getAllContatos(){
+        
+        $mysqli = new mysqli("127.0.0.1", "mariangela", "", "ticketHappy");
+        
+        $stmt = $mysqli->prepare("SELECT * FROM contatos"); //Prepara uma declaração SQL para execução
+        
+        //$stmt->bind_param("i",$_POST["id"]); //Passa variáveis para um preparado comando como parâmetros
+       
+        $stmt->execute(); //Executa uma preparada query
+        $stmt->bind_result($id, $nomeCompleto, $email, $assunto, $mensagem); //Passa variáveis para um preparado comando por resultado armazenado
+        
+        //$stmt->fetch(); //Obtém resultados de um preparado comando e os coloca nas determinadas variáveis
+        
+        while ($stmt->fetch()) {
+            
+        }
+            
+            
+        //erro
+        $row = $stmt->get_result()->fetch_all();//pega todos os resultados e joga num vetor
+        
+        foreach($row as $Contato){
+            return $c[1];
+        }
+    }
+    
+    //http://php.net/manual/pt_BR/mysqli-stmt.fetch.php
+    
+    public function updateContato($id){
+        
+        $mysqli = new mysqli("127.0.0.1", "mariangela", "", "ticketHappy");
+        
+        $stmt = $mysqli->prepare("PUT INTO contatos(nm_nomeCompleto,ds_email,ds_assunto,ds_mensagem) VALUES (?,?,?,?)");
+        
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+        $stmt->bind_result($id, $nomeCompleto, $email, $assunto, $mensagem);
+        $stmt->fetch();
+        
+        $contato = new ContatoModel($id, $nomeCompleto, $email, $assunto, $mensagem);
+        return $contato;
+    }
+    
+   
    
 
    
