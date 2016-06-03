0b8bc16b257e616610670e837de0cde5545636e9
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 19:40:17 +0000

    Mari: criando pag

---
+++ view/o_rei_leao.php
@@ -0,0 +1,74 @@
+
+	<!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+	
+		<!--------------------------- BREADCRUMB  --------------------------------------> 
+			<div class="div_breadcrumb">		
+				<ol class="breadcrumb">
+					<li><a href="/home/home">Home</a></li>
+					<li><a href="#">Teatro</a></li>
+					<li><a href="/home/musical">Musical</a></li>
+					<li class="active">O Rei Leão</li>				
+				</ol>		
+			</div>
+		<!--------------------------- BREADCRUMB -------------------------------------->
+	
+	<div id="content_imagem_fundo">
+	
+		<img class="img_fundo" src="https://ticket-happy-mariangela.c9users.io/imagens/grande/reileao_grande.jpg" alt="Imagem de fundo da pagina O Rei Leão" width="100%">	
+		
+		<div id="main_content">					
+		
+			<div class="div_transparente">
+				<h2>O REI LEÃO - O MUSICAL</h2>
+				
+				<p class="texto_justificado">
+				O espetáculo Rei Leão - O Musical, sucesso de bilheteria em 2013, 
+				reestreia no dia 8 de janeiro, no Teatro Renault. Encenada em 15 
+				países e traduzida para oito línguas diferentes, a adaptação do filme 
+				‘O Rei Leão’ fica em cartaz em São Paulo por tempo indeterminado. 
+				</p class="texto_justificado">
+				<p class="texto_justificado">Eleito em 2012 a maior bilheteria da história da Broadway, com 
+				arrecadação de US$853,8 milhões desde sua primeira apresentação, a 
+				produção já vendeu mais de 250 mil ingressos no Brasil em menos de 
+				seis meses em cartaz.</p>
+				<p class="texto_justificado">A produção apresenta Osvaldo Mil no papel de “Scar”, César Mello é 
+				“Mufasa”,Phindile Mkhize é “Rafiki”, Claudio Galvan é “Zazu”, Marcelo Klabin 
+				é “Pumba”,Ronaldo Reis é “Timão”, Tiago Barbosa é “Simba”, Josi Lopes é “Nala”, 
+				Jorge Neto é “Banzai”, Juliana Peppi é “Shenzi”, e Felippe Morais é “Ed”.</p>
+				<p class="texto_justificado">O papel do “Jovem Simba” é alternado entre Gustavo Bonfim, Henrique Filgueiras, 
+				Yudichi Taniguti e Matheus Braga, e o papel de “Jovem Nala” é alternado entre Any 
+				Gabriele, Karollyne Nascimento, Lais Dias e Ysa Paula.</p>
+			
+				<h2>INFORMAÇÕES</h2>
+				
+				<p><strong><span class="glyphicon glyphicon-calendar"></span>  Data(s):</strong> Até 14 de dezembro de 2015</p>
+				<p><strong><span class="glyphicon glyphicon-time"></span>  Horário(s):</strong> Quartas, Quintas e Sextas, às 21h | Sábados, às 16h e 21h | Domingos, às 15h30h e 20h</p>
+				<p><strong><span class="glyphicon glyphicon-piggy-bank"></span>  Preço(s):</strong> De R$50 (Balcão B) a R$280 (Setor Premium)</p>
+				<p><strong><span class="glyphicon glyphicon-map-marker"></span>  Onde:</strong> Teatro Renault
+					Avenida Brigadeiro Luís Antônio, 411 - Bela Vista</p>
+				
+			</div>
+			
+			<div class="div_transparente_video">
+				<button type="button" class="btn btn-laranja" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-facetime-video"></span>  Video</button>
+				<a href="compra_rei_leao.html" class="btn btn-laranja" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Comprar</a>
+				<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="">					
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+							<div class="imagem_video">
+								<video id="video" src="videos/rei_leao.mp4" controls loop muted preload="auto" poster="imagens/1-teatro/Infantil/1.jpg"></video> 	
+							</div>				
+					</div>
+				  </div>
+				</div>
+				</div>
+			</div>
+		
+		</div>	
+	
+	</div>
+	
+	
+	<!----------------------------------------------- FOOTER -------------------------------------------------------------------->
