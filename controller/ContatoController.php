71e7f2f926a2b544bb04494399f42eade0515f4e
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 17:39:57 +0000

    Mari: rotina de alterar funcionando

--- controller/ContatoController.php
+++ controller/ContatoController.php
@@ -7,14 +7,18 @@ class ContatoController extends Controller{
     }
    
     public function cadastroContato(){
+        $this->view->renderizar("header");
         $this->view->renderizar("contato");
+        $this->view->renderizar("footer");
     }
     // https://ticket-happy-mariangela.c9users.io/contato/cadastroContato
     
     
     //esta fazendo um redirect
     public function sucesso(){
+       $this->view->renderizar("header");
        $this->view->renderizar("sucesso"); 
+       $this->view->renderizar("footer");
     }
     
     public function inserir(){
@@ -41,9 +45,6 @@ class ContatoController extends Controller{
             $this->view->interpolar("errosql",$ret);
         } 
     }
-    // chama o controller eventoController e o formulario ==> vai chamar essa funcao quando clicar no botao
-    // https://ticket-happy-mariangela.c9users.io/evento/inserir  
-    
     
     public function listar(){
         
@@ -54,11 +55,58 @@ class ContatoController extends Controller{
         $this->view->interpolar("listar_contato",$todosContatos);
     }
     
+    // https://ticket-happy-mariangela.c9users.io/contato/listar
+    
+    // carrega os campos na pagina contatoAlterar.php
     public function alterar(){
         $id = $_GET["arg0"];
         
         $contatoDao = new ContatoDAO();
+        $contato = $contatoDao->getContato($id);
         
+        $dado["id"] = $contato->getId(); // estou trazendo o id pq na function update nao esta pegando o id pelo GET
+        
+        // MANDANDO PARA VIEW
+        $dado["nomeCompleto"] = $contato->getNomeCompleto();
+        $dado["email"]        = $contato->getEmail();
+        $dado["assunto"]      = $contato->getAssunto();
+        $dado["mensagem"]     = $contato->getMensagem(); //nao esta carregando
+        
+        $this->view->renderizar("header");
+        
+        $this->view->interpolar("contatoAlterar",$dado);
+        
+        $this->view->renderizar("footer");
+        
+    }
+    
+    public function update(){
+        //$id = $_GET["arg0"];
+        
+         //Obtem da view
+        $id = $_POST["id"];
+        
+        $nomeCompleto = $_POST["nomeCompleto"];
+        $email        = $_POST["email"];
+        $assunto      = $_POST["assunto"];
+        $mensagem     = $_POST["mensagem"];
+        
+        //passa para o ContatoModel
+        $contatoModel = new ContatoModel($id,$nomeCompleto, $email, $assunto, $mensagem);
+        $contatoDao = new ContatoDAO();
+        
+        //PASSA AO MODEL
+        $contatoDao->updateContato($contatoModel);
+        
+        header("Location: /contato/listar");
+        /*
+         if($ret === ""){
+            header("Location: /contato/listar");   
+        }else{
+            $this->view->interpolar("errosql",$ret);
+        }
+       
+        */
     }
     
     public function deletar(){
@@ -67,8 +115,8 @@ class ContatoController extends Controller{
         $contatoDao = new ContatoDAO();
         $contatoDao->deleteContato($id);
         
-        $this->view->renderizar("/contato/listar"); 
-         
+        header("Location: /contato/listar"); 
+       
     }
     
     
