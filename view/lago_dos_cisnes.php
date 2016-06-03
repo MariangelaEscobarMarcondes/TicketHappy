60463d5fdf79c30075ff21cda6d63d97660a08bd
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 19:39:17 +0000

    Mari: criando view, arrumando rotas

---
+++ view/lago_dos_cisnes.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ticket Happy - Orquestra e Dança / Lago dos Cisnes</title>
+	<link rel="shortcut icon" href="imagens/logo/favicon.ico" >
+	
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/principal.css" rel="stylesheet">
+	
+</head>
+
+<body>
+<div id="wrapper">
+
+    <!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			 
+			  <a class="navbar-brand" href="home.html">
+				<img alt="Brand" src="imagens/logo/2.png" class="img-responsive">
+			  </a>
+			</div>
+
+			<!-- MENU -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
+				<li><a href="home.html"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+				
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-triangle-bottom"></span> Teatro</a>
+				  <ul class="dropdown-menu" role="menu">
+					<li><a href="teatro_musical.html">Musical</a></li>
+					<li><a href="teatro_comedia.html">Comédia</a></li>
+					<li><a href="teatro_infantil.html">Infantil</a></li>									
+				  </ul>
+				</li>
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-triangle-bottom"></span> Shows</a>
+				  <ul class="dropdown-menu" role="menu">
+					<li><a href="shows_nacional.html">Nacional</a></li>
+					<li><a href="shows_internacional.html">Internacional</a></li>
+				  </ul>
+				</li>
+				
+				<li><a href="festivais.html"><span class="glyphicon glyphicon-triangle-right"></span>Festivais</a></li>
+				<li><a href="orquestra_danca.html"><span class="glyphicon glyphicon-triangle-right"></span>Orquestra & Dança</a></li>
+				<li><a href="exposicoes.html"><span class="glyphicon glyphicon-triangle-right"></span>Exposições</a></li>
+				<li><a href="especiais.html"><span class="glyphicon glyphicon-triangle-right"></span>Especiais</a></li>
+				
+			  </ul>
+			  
+			   <!-- LOGIN -->	
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a href="login.html"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+				
+			  </ul>
+			  
+			  <!-- FORM => SEARCH -->
+			  <form class="navbar-form navbar-right" role="search">
+				<div class="form-group">				  
+				  <input type="text" class="form-control cor_search" placeholder="Digite...">
+				</div>				
+				<button type="submit" class="btn btn-search"><span class="glyphicon glyphicon-search"></span></button>
+			  </form>
+			  
+			 
+			</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	<!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+	
+		<!--------------------------- BREADCRUMB  --------------------------------------> 
+			<div class="div_breadcrumb">		
+				<ol class="breadcrumb">
+					<li><a href="home.html">Home</a></li>
+					<li><a href="orquestra_danca.html">Orquestra e Dança</a></li>
+					<li class="active">Lago dos Cisnes</li>									
+				</ol>		
+			</div>
+		<!--------------------------- BREADCRUMB -------------------------------------->
+	
+	<div id="content_imagem_fundo">
+	
+		<img class="img_fundo"src="imagens/4-orquestra_danca/lago_dos_cisnes/foto_moscowcityballet_04.jpg" alt="Pesquisa" width="100%">		
+		
+		<div id="main_content">		 
+		
+			<div class="div_transparente">
+				<h2>O LAGO DOS CISNES</h2>
+				
+				<p class="texto_justificado">
+				"O Lago dos Cisnes", com o passar dos anos e com a evolução do ballet tem se tornado o mais
+				popular de todos. É o ballet que captura como nenhum outro todas as emoções humanas, da 
+				esperança ao desespero, do terror ao carinho, da melancolia ao êxtase. A versão que sobrevive 
+				até hoje é o sucesso que dois russos juntos coreografaram. Eram eles Marius Petipa e Lev Ivanov, 
+				o brilhante coreógrafo que desenvolveu toda a ideia dramática central de uma moça presa em um corpo 
+				de um pássaro, sendo assim representado pela leveza dos movimentos dos braços da bailarina, 
+				as articulações de seus pés e a posição de sua cabeça e o alongamento do pescoço. 
+				</p>
+				
+				<p class="texto_justificado">"O Lago dos Cisnes" conta a história do jovem Príncipe Siegfried, 
+				que se apaixona por Odette, uma rainha que foi transformada em um cisne por um feiticeiro malvado. 
+				Odette o explica que ela é destinada a permanecer como esta estranha criatura, até ser resgatada 
+				por um homem que jure amor eterno a ela. Encantado com a beleza dela, o Príncipe promete jurar 
+				amor eterno ao mundo. Mas logo em seguida, na festa em sua homenagem por seu 21º aniversário ele
+				é enganado pelo feiticeiro, von Rothbart, declarando seu amor por Odile, uma gêmea malvada de 
+				Odette, o levando a jurar amor por ela. Quando ele se deu por si, correu de volta ao lago. Lá 
+				ele combate Von Rothbart e destrói seu poder e assim os apaixonados podem ficar juntos e felizes 
+				para sempre. Este é um dos mais difíceis balés tecnicamente, em parte pela honrosa bailarina 
+				italiana chamada Pierina Legnani. </p>
+				
+			
+				<h2>INFORMAÇÕES</h2>
+				
+				<p><strong><span class="glyphicon glyphicon-calendar"></span>  Data(s):</strong> Até 14 de dezembro de 2015</p>
+				<p><strong><span class="glyphicon glyphicon-time"></span>  Horário(s):</strong> Quinta a sábado 20h | Domingo 17h</p>
+				<p><strong><span class="glyphicon glyphicon-piggy-bank"></span>  Preço(s):</strong>  R$30</p>
+				<p><strong><span class="glyphicon glyphicon-map-marker"></span>  Onde:</strong> Centro Deborah Colker
+					Rio de Janeiro - RJ</p>
+				
+			</div>
+						
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-laranja" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-facetime-video"></span>  Video</button>
+			<a href="compra_lago.html" class="btn btn-laranja" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Comprar</a>
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			  <div class="modal-dialog">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-wrench"></span>  Ops...</h4>
+				  </div>
+				  <div class="modal-body">
+					<img src="imagens/pagina_em_construcao.png" alt="" class="img-responsive">
+				  </div>
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-laranja" data-dismiss="modal">Close</button>					
+				  </div>
+				</div>
+			  </div>
+			</div>
+		
+	</div>	
+	
+	
+	
+		
+	</div>	
+	
+	
+	<!----------------------------------------------- FOOTER -------------------------------------------------------------------->
+	 <footer id="footer" class="clearfix">
+      <div id="footer-widgets">
+
+        <div class="container">
+
+        <div id="footer-wrapper">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+				<div id="meta-3" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Serviços</h4>
+					<ul>
+						<li><a href="contato.html">Contato</a></li>
+						<li><a href="#">Central de Vendas</a></li>
+						<li><a href="#" title="">Vendas para Grupos</a></li>
+						<li><a href="#" title="">Vendas pela Internet</a></li>
+						<li><a href="#" title="">Vantagens do Sistema</a></li>						
+					</ul>
+				</div>            
+			</div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-3">
+              	<div id="recent-posts-3" class="widget widgetFooter widget_recent_entries"><h4 class="widgettitle">Formas de Pagamento</h4>	
+					<div class="form-inline">				
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/pagamento.png" alt="Formas de Pagamento" ></a>							
+						</div>
+						
+				</div>
+				</div>           
+			</div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-3">
+				<div id="meta-4" class="widget widgetFooter widget_meta"><h4 class="widgettitle">Certificados de Segurança</h4>		
+					<div class="form-inline">				
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/certificados.png" alt="Cerificados de Segurança"></a>							
+						</div>
+							
+				</div>
+				</div>           
+			</div> <!-- end widget1 -->
+			<!------------ 4 ----------------->
+             <div class="col-sm-6 col-md-3">
+				<div id="search-3" class="widget widgetFooter widget_search"><h4 class="widgettitle">Mídias Sociais</h4>
+				<div class="form-inline">				
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/1.jpg" alt="Imagem icone Facebook"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/2.jpg" alt="Imagem icone Twitter"></a>								
+						</div>	
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/3.jpg" alt="Imagem icone Google +"></a>							
+						</div>
+						<div class="input-group">							
+							<a href="#"><img src="imagens/icones/4.jpg" alt="Imagem icone Youtube"></a>								
+						</div>	
+				</div>
+				</div>          
+			</div> <!-- end 4 -->
+
+          </div> <!-- end .row -->
+
+        </div> <!-- end #footer-wrapper -->
+
+        </div> <!-- end .container -->
+      </div> <!-- end #footer-widgets -->
+
+		<div id="sub-floor">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 copyright">
+						tickethappy.com.br 2015 © 
+					</div>	
+
+					<div class="col-md-4 col-md-offset-4 attribution">
+						Desenvolvido por:<a href="#"> DMR</a>	
+					</div>
+
+				</div> <!-- end .row -->
+			</div>
+		</div>
+
+    </footer> <!-- end footer -->
+	<!----------------------------------------------- FOOTER -------------------------------------------------------------------->
+	
+	
+</div><!-- WRAPPER -->
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+</body>
+</html>
