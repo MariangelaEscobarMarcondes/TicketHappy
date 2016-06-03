f990519755cdef28b02fde267f5222516d2e7f5e
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-03 19:37:54 +0000

    Mari: criando view, arrumando rotas

---
+++ view/la_nouba.php
@@ -0,0 +1,69 @@
+
+	<!----------------------- NAVBAR => BOOSTRAP --------------------------------------------------------->
+	
+	
+	<div id="content_imagem_topo">
+		<div id="content_imagem_topo">	
+			<img src="https://ticket-happy-mariangela.c9users.io/imagens/carousel/5-La_Nouba.jpg" alt="Pesquisa" width="100%">		
+		</div>	
+		
+		<div class="div_breadcrumb div_breadcrumb_cold">
+			<!--------------------------- BREADCRUMB  --------------------------------------> 
+			<ol class="breadcrumb">
+				<li><a href="/home/home">Home</a></li>
+				<li><a href="/home/especiais">Especiais</a></li>				
+				<li class="active">La Nouba</li>
+					
+			</ol>
+			<!--------------------------- BREADCRUMB --------------------------------------> 
+		</div>	
+	
+		<div id="main_content">
+			<div class="div_info">
+				<h2>La Nouba</h2>
+				
+				<p class="texto_justificado">La Nouba é um espetáculo do Cirque du 
+				Soleil que está fixamente em cartaz no teatro do Walt Disney World em Orlando, 
+				nos Estados Unidos (mais propriamente dizendo está situado no centro de entretenimento
+				Downtown Disney). O espetáculo faz uma narrativa sobre os sonhos humanos, passando por 
+				diversas situações inusitadas que podem ocorrer em sonhos.
+				</p>
+				
+				
+				
+				<h2>INFORMAÇÕES</h2>
+				
+				<p><strong><span class="glyphicon glyphicon-calendar"></span>  Data(s):</strong> A partir de 5 de julho de 2015</p>
+				<p><strong><span class="glyphicon glyphicon-time"></span>  Horário(s):</strong> Quinta e sexta-feira, às 21h | Sábado, às 17h e 21h | Domingo, às 16h e 20h</p>
+				<p><strong><span class="glyphicon glyphicon-piggy-bank"></span>  Preço(s):</strong> De R$25 a R$260</p>				
+				<p><strong><span class="glyphicon glyphicon-map-marker"></span>  Onde:</strong>  Ginásio Nilson Nelson - Brasilia</p>
+						
+			</div>			
+					
+			
+			
+			<div class="div_transparente_video">
+				<button type="button" class="btn btn-laranja" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-facetime-video"></span>  Video</button>
+				<a href="compra_la_nouba.html" class="btn btn-laranja" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Comprar</a>
+				<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg">
+					<div class="">					
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+							<div class="imagem_video">
+								<video id="video" src="videos/la_nouba.mp4" controls loop muted preload="auto" poster="https://ticket-happy-mariangela.c9users.io/imagens/6-especiais/Cirque_du_soleil_LaNouba/1.jpg"></video> 	
+							</div>				
+					</div>
+				  </div>
+				</div>
+				</div>
+			</div>
+		
+		
+		</div><!-- CONTENT -->
+		
+				
+		
+		 
+	</div>	
+	<!----------------------------------------------- FOOTER -------------------------------------------------------------------->
