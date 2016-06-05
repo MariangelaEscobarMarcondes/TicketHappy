cc978841c644a89bb0ec3db4ecbfcce7d6f0dcd7
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-05 21:22:39 +0000

    Mari:criando view e carregando variavel de session

--- view/logout.php
+++ view/logout.php
@@ -1,11 +1,54 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Logout</title>
-    </head>
-    <body>
-        <h1>Adeus</h1>
-    </body>
-</html>
 
-
+    <!-- HEADER -->
+	<!-- NAVBAR => BOOSTRAP -->
+	
+	<div class="div_breadcrumb">
+		<!-- BREADCRUMB  --> 
+		<ol class="breadcrumb">
+			<li><a href="home.html">Home</a></li>
+			<li class="active">Logout</li>				
+		</ol>
+		<!-- BREADCRUMB --> 
+		<p style="color:#FFFF99">Tchau:  <?= $dado["nome"] ?></p>
+		<p style="display:none">Id:     <?= $dado["id"] ?></p>
+	</div>	
+	
+	<div id="content">
+		<div style="clear: both"></div>
+					  
+				
+		
+		<main id="largura_contato_form">	
+			
+		
+			
+				
+			<form method="post" action="/login/autenticar" class="form-horizontal">
+				
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<img src="https://ticket-happy-mariangela.c9users.io/imagens/garfield35tchau.gif" alt="" class="img_login img-responsive">	
+					</div>
+				</div>
+				
+			
+				
+			
+				
+			</form>
+			
+			<br><br><br><br>
+		</main>
+		<!-- MAIN -->
+		
+		<!-- ASIDE -->
+		<div id="aside_contato">
+					
+		</div>
+		<!-- ASIDE -->
+		
+		<div style="clear: both"></div>
+	</div>
+	
+	
+		<!-- FOOTER -->
