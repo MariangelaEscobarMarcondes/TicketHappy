febc513a2981472f6f3f4b83b1708bd23d904aa2
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-05 11:39:31 +0000

    Mari: criei todas as rotas para as pags que nao possuem model e DAO

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -2,40 +2,91 @@
 
 class HomeController extends Controller{
     
-    /*public function __call($m,$a){
-        require_once "view/View.php";
-        $vi = new View();
-        $vi->renderizar("erro");
-    }*/
-    
-    /*
-    public function ola(){
-        require_once "view/View.php";//tah chamando do app
-        $vi = new View();
-        //$this->view->load("ola");
-        $vi->renderizar("ola");
-    }
-    */
-    
-
-    
     public function __call($m,$a){
         $this->view->renderizar("erro");
     }
-    // /home/ola
-    public function ola(){
-        $this->view->renderizar("ola");
-        $_SESSION["_OI"] = "Ola";
+    
+    public function home(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("home");
+        $this->view->renderizar('footer'); //chama o footer.php
     }
-    // /home/formulario
-    public function formulario(){
-        $this->view->renderizar("userform");
+    
+    public function especiais(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("especiais");
+        $this->view->renderizar('footer');
     }
-    // /home/outro
-    public function outro(){
-        echo $_POST["nome"] . "<br>";
-        echo $_SESSION["_OI"];
+    
+    public function exposicoes(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("exposicoes");
+        $this->view->renderizar('footer');
     }
+    
+    public function festivais(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("festivais");
+        $this->view->renderizar('footer');
+    }
+    
+    public function orquestraEdanca(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("orquestra_danca");
+        $this->view->renderizar('footer');
+    }
+    
+    public function internacional(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("shows_internacional");
+        $this->view->renderizar('footer');
+    }
+    
+    public function nacional(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("shows_nacional");
+        $this->view->renderizar('footer');
+    }
+    
+    public function comedia(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("teatro_comedia");
+        $this->view->renderizar('footer');
+    }
+    
+    public function infantil(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("teatro_infantil");
+        $this->view->renderizar('footer');
+    }
+    
+    public function musical(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("teatro_musical");
+        $this->view->renderizar('footer');
+    }
+    
+    public function laNouba(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("la_nouba");
+        $this->view->renderizar('footer');
+    }
+    
+    public function oReiLeao(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("o_rei_leao");
+        $this->view->renderizar('footer');
+    }
+    
+    public function coldplay(){
+        $this->view->renderizar("header");
+        $this->view->renderizar("coldplay");
+        $this->view->renderizar('footer');
+    }
+    
+    
+    
+    
        
    
     
