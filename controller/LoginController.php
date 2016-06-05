515a34115f937e0be08bb354accebd78f9adfd35
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-05 13:38:17 +0000

    Mari: autenticacao de usuario ok

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -18,52 +18,65 @@ class LoginController extends Controller{
     public function formulario(){
         //apagar a sessao
         unset($_SESSION["_ID"]);
+        
+        $this->view->renderizar("header");
         $this->view->renderizar("login");
+        $this->view->renderizar('footer');
     }
     //PRIMEIRO CHAMA ESSE CAMINHO PARA PODER CHAMAR O FORMULARIO DE LOGIN.... OK 
     // https://ticket-happy-mariangela.c9users.io/login/formulario 
    
-    
     public function autenticar(){
        
         //Obtem da view
         $email = $_POST["email"]; // o email eh o login
         $senha = $_POST["senha"];
-    
-        // Gera um hash baseado em bcrypt
-        $senha = 'senha';
-        $custo = '08';       
-        $salt = 'Cf1f11ePArKlBJomM0F6aJ';      
-        $hash = crypt($senha, '$2a$' . $custo . '$' . $salt . '$');
         
-        /*
-        * Para verificar, ele deve buscar no banco de dados a hash armazenada 
-        * no campo da senha do usuario (campo de 60 caracteres = VARCHAR (60))
-        */
+         $options = [
+             'cost' => 11,
+             'salt' => 'Cf1f11ePArKlBJomM0F6aJ',
+        ];
         
-        //passa para o LoginModel
-        $loginModel = new LoginModel(0, $email, $senha); //nao esta achando
+        $senhaHash =  password_hash($senha, PASSWORD_BCRYPT, $options);
+        
+        $loginModel = new LoginModel(0,$email,"", $senhaHash);
         
         $loginDAO = new LoginDAO();
         
         //PASSA AO DAO
         $ehLoginCorreto = $loginDAO->authUsuario($loginModel);
         
-        /*
-        $loginDAO = new LoginDAO();
-
-        $ehLoginCorreto = $loginDAO->authUsuario($email,$hash);   //esta dando erro nessa linha
-        */
-        
+    
         //sera falsa se nao encontrar
         //sera o id se encontrar
         if($ehLoginCorreto === false){
             header("Location: /login/formulario");
         }else{
             $_SESSION["_ID"] = $ehLoginCorreto; 
-            header("Location: /usuario/perfil/" . $ehLoginCorreto);
+           
+            header("Location: /login/perfil/" . $ehLoginCorreto);
+           
         }
     }
+    
+     public function perfil(){
+        $id = $_GET["arg0"];
+        
+        //PEGANDO DADOS DO MODEL
+        $loginDao = new LoginDAO();
+        $usuario = $loginDao->getUsuario($id);
+        // -----------------------------
+        
+        // MANDANDO PARA VIEW
+        $dado["id"] = $usuario->getId();
+        $dado["nome"] = $usuario->getNome();
+        
+        $this->view->renderizar("header");
+        $this->view->interpolar("perfil",$dado);
+        $this->view->renderizar("footer");
+        // ------------------------------
+    }
+    
     
 }  
 
