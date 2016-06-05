bd0ff5ab459f609106b0f370ff6f3c0fa03411b1
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-05 13:33:02 +0000

    Mari: carrega na view usuario logado (nome e id)

--- view/perfil.php
+++ view/perfil.php
@@ -1,10 +1,34 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <p>Login: <?= $dado["login"] ?></p>
-        <p>Nome:  <?= $dado["nome"] ?></p>
-    </body>
-</html>
+
+    <!-- HEADER -->
+	<!-- NAVBAR => BOOSTRAP -->
+	
+	<div class="div_breadcrumb">
+		<!-- BREADCRUMB  --> 
+		<ol class="breadcrumb">
+			<li><a href="home.html">Home</a></li>
+			<li class="active">Inserido com sucesso</li>				
+		</ol>
+		<!-- BREADCRUMB --> 
+		<p style="color:#FFFF99">Bem Vindo:  <?= $dado["nome"] ?></p>
+		<p style="display:none">Id:     <?= $dado["id"] ?></p>
+	</div>	
+	
+	<div id="content">
+		<div style="clear: both"></div>
+		
+		<!-- MAIN -->
+		<main id="largura_contato_form">
+			<h1>Logado com sucesso !!!</h1>
+			<br><br>
+			
+			
+          
+		
+		</main>
+		<!-- MAIN -->
+		
+		<div style="clear: both"></div>
+	</div>
+	
+	
+		<!-- FOOTER -->
