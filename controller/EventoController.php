16776046896e1339640bc26e52114c3f82c1b277
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-07 18:01:54 +0000

    Mari: criando controller

--- controller/EventoController.php
+++ controller/EventoController.php
@@ -1,95 +1,129 @@
 <?php
-
 class EventoController extends Controller{
     
     public function __call($m,$a){
         $this->view->renderizar("erro");
     }
    
-    public function cadastroEventos(){
+    public function cadastroEvento(){
+        $this->view->renderizar("header");
         $this->view->renderizar("cadastro_eventos");
+        $this->view->renderizar("footer");
     }
-    // https://ticket-happy-mariangela.c9users.io/evento/cadastroEventos
+    // https://ticket-happy-mariangela.c9users.io/evento/cadastroEvento
     
     
     //esta fazendo um redirect
     public function sucesso(){
+       $this->view->renderizar("header");
        $this->view->renderizar("sucesso"); 
+       $this->view->renderizar("footer");
     }
     
     public function inserir(){
         
         //Obtem da view
-        $evento      = $_POST["evento"];
-        $local       = $_POST["local"];
-        $cidade      = $_POST["cidade"];
-        $categoria   = $_POST["categoria"];
-        $texto       = $_POST["texto"];
-        $data        = $_POST["data"];
-        $horario     = $_POST["horario"];
-        $preco       = $_POST["preco"];
-        $onde        = $_POST["onde"];
-        $imagem      = $_POST["imagem"];
-        $imagemFundo = $_POST["imagemFundo"];
+        $evento     = $_POST["evento"];
+        $valor      = $_POST["valor"];
+        $quantidade = $_POST["quantidade"];
+        $categoria  = $_POST["categoria"];
+       
         
         //ignorar id = autoincremento
         
-        //passa para o EventoModel
-        $eventoModel = new EventoModel(0, $evento, $local, $cidade, $categoria, $texto, $data, $horario, $preco, $onde, $imagem, $imagemFundo);
+       
+        $eventoModel = new EventoModel(0, $evento, $valor, $quantidade, $categoria);
         $eventoDao = new EventoDAO();
         
         //PASSA AO MODEL
         $ret = $eventoDao->insert($eventoModel);
         
         if($ret === ""){
-            header("Location: /evento/sucesso");    
+            header("Location: /contato/sucesso");    
         }else{
             $this->view->interpolar("errosql",$ret);
         } 
     }
-    // chama o controller eventoController e o formulario ==> vai chamar essa funcao quando clicar no botao
-    // https://ticket-happy-mariangela.c9users.io/evento/inserir  
-    
-    
-      public function deletar(){
-        $id = $_GET["arg0"];
-       
-        $eventoDao = new EventoDAO();
-        $eventoDao->deleteEvento($id);
-         
-    }
     
     public function listar(){
-        $id = $_GET["arg0"];
         
-        //PEGANDO DADOS DO MODEL
         $eventoDao = new EventoDAO();
-        $evento = $eventoDao->getAllEvento($id);
-        // -----------------------------
         
-        // MANDANDO PARA VIEW
-        $dado["evento"] = $evento->getEvento();
-        $dado["local"] = $evento->getLocal();
-        $dado["cidade"] = $evento->getCidade();
-        $dado["categoria"] = $evento->getCategoria();
+        $todosEventos = $eventoDao->getEventos();
         
-        $this->view->interpolar("listar",$dado);
-        // ------------------------------
+        $this->view->renderizar("header");
+        $this->view->interpolar("listar_eventos",$todosEventos);
+        $this->view->renderizar("footer");
     }
-    //https://ticket-happy-mariangela.c9users.io/evento/listar/1
     
+    // https://ticket-happy-mariangela.c9users.io/eventos/listar
+    
+    // carrega os campos na pagina contatoAlterar.php
     public function alterar(){
         $id = $_GET["arg0"];
         
+        $eventoDao = new EventoDAO();
+        $evento = $eventoDao->getEvento($id);
+        
+        $dado["id"] = $evento->getId(); // estou trazendo o id pq na function update nao esta pegando o id pelo GET
+        
+        // MANDANDO PARA VIEW
+        $dado["evento"]     = $evento->getEvento();
+        $dado["valor"]      = $evento->getValor();
+        $dado["quantidade"] = $evento->getQuantidade();
+        $dado["categoria"]  = $evento->getCategoria(); //nao esta carregando
+        
+        $this->view->renderizar("header");
+        
+        $this->view->interpolar("eventoAlterar",$dado);
+        
+        $this->view->renderizar("footer");
         
     }
+    
+    public function update(){
+        //$id = $_GET["arg0"];
+        
+         //Obtem da view
+        $id = $_POST["id"];
+        
+        $evento     = $_POST["evento"];
+        $valor      = $_POST["valor"];
+        $quantidade = $_POST["quantidade"];
+        $categoria  = $_POST["categoria"];
+        
+        //passa para o ContatoModel
+        $eventoModel = new EventoModel($id,$evento, $valor, $quantidade, $categoria);
+        $eventoDao = new EventoDAO();;
+        
+        //PASSA AO MODEL
+        $eventoDao->updateEvento($eventoModel);
+        
+        header("Location: /evento/listar");
+        /*
+         if($ret === ""){
+            header("Location: /contato/listar");   
+        }else{
+            $this->view->interpolar("errosql",$ret);
+        }
+       
+        */
+    }
+    
+    public function deletar(){
+        $id = $_GET["arg0"]; //pega id via url
+       
+        $eventoDao = new EventoDAO();
+        $eventoDao->deleteEvento($id);
+        
+        header("Location: /evento/listar"); 
+       
+    }
+    
     
     
    
     
 }
-
-
-
 ?>
 
