e2fbf2095a66cd37f6659d75284980d083af5c31
mariangela <mari.escobar.marcondes@hotmail.com>
2016-06-08 23:17:49 +0000

    Mari: retirando cod que nao estava sendo usado

--- view/listar_contato.php
+++ view/listar_contato.php
@@ -53,27 +53,7 @@
 			
 			<br><br>
 			
-			<div id="centro">
-				<nav>
-				  <ul class="pagination">
-				    <li>
-				      <a href="#" aria-label="Previous">
-				        <span aria-hidden="true">&laquo;</span>
-				      </a>
-				    </li>
-				    <li><a href="#">1</a></li>
-				    <li><a href="#">2</a></li>
-				    <li><a href="#">3</a></li>
-				    <li><a href="#">4</a></li>
-				    <li><a href="#">5</a></li>
-				    <li>
-				      <a href="#" aria-label="Next">
-				        <span aria-hidden="true">&raquo;</span>
-				      </a>
-				    </li>
-				  </ul>
-				</nav>
-			</div>
+
 			
 		</main>
 		<!-- MAIN -->
